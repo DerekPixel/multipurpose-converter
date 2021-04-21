a2ec19984482ec75a240a008c827047168fd424e
Derek Price <derekp098@gmail.com>
2021-04-20 23:47:38 -0700

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header>
+          <h1>Converter</h1>
+          <div className="creds-div">
+            <p className="cred">Made by Derek Price</p>
+            <a className="cred github" target="_blank" href="https://github.com/DerekPixel/multipurpose-converter">GitHub</a>
+          </div>
+        </header>
+    </div>
+  );
+}
+
+export default App;
